855d4fb8863e60c39860da965d2e981228c20572
Daniel Bamba <djbamba@live.com>
2021-09-20 17:16:10 -0500

    Separate WebClient configuration.

--- src/main/java/com/github/djbamba/service/weather/api/service/WeatherService.java
+++ src/main/java/com/github/djbamba/service/weather/api/service/WeatherService.java
@@ -4,7 +4,6 @@ import com.github.djbamba.service.weather.api.response.WeatherResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
@@ -13,34 +12,22 @@ import reactor.core.publisher.Mono;
 @Service
 public class WeatherService {
   public final Logger log = LoggerFactory.getLogger(WeatherService.class);
-  private final WebClient client;
 
-  @Value("${api.key}")
-  private String apiKey;
+  @Autowired private final WebClient client;
 
-  @Value("${api.base.url}")
-  private String apiUrl;
-
-  @Autowired
-  public WeatherService(WebClient.Builder wcBuilder) {
-    this.client = wcBuilder.baseUrl(apiUrl).build();
+  public WeatherService(WebClient client) {
+    this.client = client;
   }
 
   public Mono<WeatherResponse> findWeatherByZip(String zip, String units) {
     ResponseSpec currentWeather =
         this.client
             .get()
-            .uri(
-                apiUrl,
-                uriBuilder ->
-                    uriBuilder
-                        .queryParam("zip", zip)
-                        .queryParam("units", units)
-                        .queryParam("appid", apiKey)
-                        .build())
+            .uri(uriBuilder -> uriBuilder.queryParam("zip", zip).queryParam("units", units).build())
             .retrieve();
 
-    log.debug("ZIP: {} KEY: {}", zip, apiKey);
+    log.debug("ZIP: {}", zip);
+
     return currentWeather.bodyToMono(WeatherResponse.class);
   }
 }
