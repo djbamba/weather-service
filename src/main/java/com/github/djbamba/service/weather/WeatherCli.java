6013aab8ae3a7c0cb2fb1063f018a5ef8c4c546d
Daniel Bamba <djbamba@live.com>
2021-10-04 16:22:28 -0500

    Adding methods for seeding/clearing.

--- src/main/java/com/github/djbamba/service/weather/WeatherCli.java
+++ src/main/java/com/github/djbamba/service/weather/WeatherCli.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.djbamba.service.weather.api.model.City;
-import com.github.djbamba.service.weather.repo.MongoCityRepository;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -13,9 +12,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.TextIndexDefinition;
+import org.springframework.data.mongodb.core.index.TextIndexDefinition.TextIndexDefinitionBuilder;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(1)
 public class WeatherCli implements CommandLineRunner {
 
   private Logger log = LoggerFactory.getLogger(WeatherCli.class);
@@ -23,23 +28,32 @@ public class WeatherCli implements CommandLineRunner {
   @Value("${populate.cities:false}")
   private boolean populateCityCollection;
 
-  @Autowired private MongoCityRepository cityRepository;
+  @Autowired private MongoOperations mongoOps;
 
   @Override
   public void run(String... args) {
+    if (populateCityCollection) {
+      clear();
+      seed();
+    }
+  }
+
+  private void clear() {
+    this.mongoOps.remove(new Query(), City.class);
+  }
+
+  private void seed() {
     ObjectMapper mapper = new ObjectMapper();
     mapper.configure(Feature.AUTO_CLOSE_SOURCE, true);
+    TextIndexDefinition cityNameAndState =
+        new TextIndexDefinitionBuilder().onField("name", 1.1F).onField("state").build();
 
-    if (populateCityCollection) {
-      try (InputStream is =
-          WeatherCli.class.getClassLoader().getResourceAsStream("city.list.json")) {
-
-        List<City> cities = mapper.readValue(is, new TypeReference<List<City>>() {});
-        cityRepository.bulkInsert(cities);
-
-      } catch (IOException e) {
-        log.error("Reading city.list.json", e);
-      }
+    try (InputStream is = WeatherCli.class.getClassLoader().getResourceAsStream("city.list.json")) {
+      List<City> cities = mapper.readValue(is, new TypeReference<List<City>>() {});
+      this.mongoOps.insertAll(cities);
+      this.mongoOps.indexOps(City.class).ensureIndex(cityNameAndState);
+    } catch (IOException e) {
+      log.error("Reading city.list.json", e);
     }
   }
 }
